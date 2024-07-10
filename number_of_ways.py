25e080d18179f827e1b595f0816270c6f86a6763
Ankita Kundu <akundu2@andrew.cmu.edu>
2024-07-10 14:44:58 -0400

    Fix initialization of paths

--- number_of_ways.py
+++ number_of_ways.py
@@ -20,28 +20,28 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
 
     # loop k times
     for i in tqdm(range(k)):
-        for path in paths:
-            new_path = path.copy()
+        for j in range(len(paths)):
+            new_path = paths.pop()
             last_position = new_path[-1]
 
             # exist fast if not going to make to end
-            if endPos - last_position > (k - i - 1):
+            if endPos - last_position > (k - i):
                 continue
             # path that goes to the left
             new_path_left = new_path + [last_position - 1]
 
             # path that goes to the right
-            new_path_right = new_path + [last_position - 1]
+            new_path_right = new_path + [last_position + 1]
 
             # add paths to the left and right
             paths.append(new_path_left)
             paths.append(new_path_right)
 
-    num_ways = 0
+    new_ways = 0
     for path in paths:
         if path[-1] == endPos:
             new_ways += 1
-    return num_ways
+    return new_ways
 
 
 def test_number_of_ways():
